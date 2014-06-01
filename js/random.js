55a2e5fe37e7bc3e1e77978c7f06020a7b786301
Armando <flores@mtu.edu>
2014-05-31 23:48:37 -0400

    A Bunch of changes

--- js/random.js
+++ js/random.js
@@ -1,3 +1,22 @@
+// Callout new div after old one... 
+// $(function() {
+  // $('#cats').isvisible(function() {
+   //  alert("do something");
+  // });
+// });
+
+
+
+
+  //          $('#ycon').load(function () {
+               
+     //           $('#up-pic').show( function() {
+      //              alert('hey boo');
+        //        });
+     //       });
+
+
+
 
 // Randomizer
 var randomNumGen = Math.floor((Math.random() * 10) + 1);
@@ -7,3 +26,28 @@ var randomNumGen = Math.floor((Math.random() * 10) + 1);
     } else {
         document.getElementById("writings").style.display = "none";
     }
+
+
+
+
+
+
+ var newDivAfterPicUpload = document.createElement('div');
+    $('#learningbox').append(newDivAfterPicUpload);
+
+
+// Function to change the content of t2
+function modifyText() {
+  var t2 = document.getElementById("t2");
+  if (t2.firstChild.nodeValue == "three") {
+    t2.firstChild.nodeValue = "two";
+  } else {
+    t2.firstChild.nodeValue = "three";
+  }
+}
+
+// add event listener to t
+var el = document.getElementById("outside");
+el.addEventListener("click", modifyText, false);
+
+

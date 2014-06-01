84ef32fac9109b128cfdb7ba2b5138f7a68fbafa
Armando <flores@mtu.edu>
2014-06-01 14:33:38 -0700

    Added a lot more stuff
    IT’s fairly close to being done!

--- js/random.js
+++ js/random.js
@@ -16,6 +16,44 @@
      //       });
 
 
+  //function newBlankDiv(){
+  //EventEmitter.call(this);
+  // custom initialization here}
+  //{ newBlankDiv.prototype = new EventEmitter;}
+
+
+
+
+
+// DROPZONE JQUERY FIX THAT DOESNT WORK
+
+
+   //           var imgURL = "";
+
+      //        $("a, img").on("mousedown", function(e) {
+      //            var $target = $(e.target);
+      //            if ($target.prop("tagName").toLowerCase() == 'img') {
+      //                imgURL = $target.attr('src');
+      //            }
+   //           });
+
+      //        $("#my-drop-area").on("dragenter dragover dragleave", function(e) {
+                  // need for proper ondrop event activation
+     //             e.preventDefault();
+      //            e.stopPropagation();
+      //        }).on("drop", function(e) {
+      //            e.preventDefault();
+      //            e.stopPropagation();
+      //            console.debug("Dropped! URL:", imgURL);
+      //        });
+
+
+
+
+
+
+
+
 
 
 // Randomizer
@@ -28,26 +66,42 @@ var randomNumGen = Math.floor((Math.random() * 10) + 1);
     }
 
 
+//set a variable to refer to a specific ID
+var uppic = document.getElementById("uppic");
+//initiate this function when the user clicks the ID
+uppic.onclick = function() {
+      alert("Hello World");
+};
 
 
 
 
- var newDivAfterPicUpload = document.createElement('div');
-    $('#learningbox').append(newDivAfterPicUpload);
+
+
+
+
+
+  //       function blankBox() {
+     //        var  uppic = document.getElementById("uppic");
+                  
+        //          if (uppic.load == "true") {
+           //           var newDivAfterPicUpload = document.createElement('div');
+          //            $('#learningbox').append(newDivAfterPicUpload);
+                  //   }          }
 
 
 // Function to change the content of t2
-function modifyText() {
-  var t2 = document.getElementById("t2");
-  if (t2.firstChild.nodeValue == "three") {
-    t2.firstChild.nodeValue = "two";
-  } else {
-    t2.firstChild.nodeValue = "three";
-  }
-}
+   //      function modifyText() {
+    //       var t2 = document.getElementById("t2");
+//           if (t2.firstChild.nodeValue == "three") {
+   //          t2.firstChild.nodeValue = "two";
+    //       } else {
+     //        t2.firstChild.nodeValue = "three";
+       //    }
+    //     }
 
 // add event listener to t
-var el = document.getElementById("outside");
-el.addEventListener("click", modifyText, false);
+// var el = document.getElementById("outside");
+// el.addEventListener("click", modifyText, false);
 
 

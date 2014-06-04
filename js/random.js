3b79b09504053b6f37bb3e4ce22824a53d4244fb
Armando <flores@mtu.edu>
2014-06-03 22:54:03 -0700

    Cat Img / Got the .onclick to work jquery
    This is pretty much just to continue my record. :)
    And some few changes…
    
    Not quite what I wanted but, it’s baby steps… Small potatoes

--- js/random.js
+++ js/random.js
@@ -1,59 +1,3 @@
-// Callout new div after old one... 
-// $(function() {
-  // $('#cats').isvisible(function() {
-   //  alert("do something");
-  // });
-// });
-
-
-
-
-  //          $('#ycon').load(function () {
-               
-     //           $('#up-pic').show( function() {
-      //              alert('hey boo');
-        //        });
-     //       });
-
-
-  //function newBlankDiv(){
-  //EventEmitter.call(this);
-  // custom initialization here}
-  //{ newBlankDiv.prototype = new EventEmitter;}
-
-
-
-
-
-// DROPZONE JQUERY FIX THAT DOESNT WORK
-
-
-   //           var imgURL = "";
-
-      //        $("a, img").on("mousedown", function(e) {
-      //            var $target = $(e.target);
-      //            if ($target.prop("tagName").toLowerCase() == 'img') {
-      //                imgURL = $target.attr('src');
-      //            }
-   //           });
-
-      //        $("#my-drop-area").on("dragenter dragover dragleave", function(e) {
-                  // need for proper ondrop event activation
-     //             e.preventDefault();
-      //            e.stopPropagation();
-      //        }).on("drop", function(e) {
-      //            e.preventDefault();
-      //            e.stopPropagation();
-      //            console.debug("Dropped! URL:", imgURL);
-      //        });
-
-
-
-
-
-
-
-
 
 
 // Randomizer
@@ -66,41 +10,13 @@ var randomNumGen = Math.floor((Math.random() * 10) + 1);
     }
 
 
-//set a variable to refer to a specific ID
-var uppic = document.getElementById("uppic");
-//initiate this function when the user clicks the ID
-uppic.onclick = function() {
-      alert("Hello World");
-};
 
 
+//The . click to annon function 
 
-
-
-
-
-
-  //       function blankBox() {
-     //        var  uppic = document.getElementById("uppic");
-                  
-        //          if (uppic.load == "true") {
-           //           var newDivAfterPicUpload = document.createElement('div');
-          //            $('#learningbox').append(newDivAfterPicUpload);
-                  //   }          }
-
-
-// Function to change the content of t2
-   //      function modifyText() {
-    //       var t2 = document.getElementById("t2");
-//           if (t2.firstChild.nodeValue == "three") {
-   //          t2.firstChild.nodeValue = "two";
-    //       } else {
-     //        t2.firstChild.nodeValue = "three";
-       //    }
-    //     }
-
-// add event listener to t
-// var el = document.getElementById("outside");
-// el.addEventListener("click", modifyText, false);
-
-
+$( "#whole-uploader" ).on( "click", function (){
+         $(".refresh").show("slow");
+         $(".col-sm-6 > .blankdiv").show("slow");
+         $(".dowork").show("slow");
+});
+      
